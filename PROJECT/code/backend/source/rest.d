e5a10e86cfe42b49ec02e7f14d90addc471b45f9
Eupheme <hannarainbowmail@gmail.com>
2016-12-25 11:15:11 +0100

    login in logout

--- PROJECT/code/backend/source/rest.d
+++ PROJECT/code/backend/source/rest.d
@@ -3,11 +3,18 @@ import biblios.db;
 import vibe.d;
 
 import std.random;
+import std.algorithm : canFind;
+import std.stdio;
+
+enum authCookie = headerParam("auth", "Cookie");
 
 @path("/api")
 interface IAPI
 {
 	string postLogin(string username, string password);
+
+	@authCookie
+	void postLogout(string auth = "");
 }
 
 class APIImpl : IAPI
@@ -25,14 +32,43 @@ class APIImpl : IAPI
 		}
 		return str;
 	}
+
+	private string parseCookie(string cookie, string key)
+	{
+		writeln("\nParsing...");
+		foreach(string c; cookie.split(";"))
+		{
+			string[] arr = c.split("=");
+			foreach (string s; arr)
+				writeln("\"" ~ s.strip() ~ "\"");
+			if (arr[0].strip() == key)
+				return arr[1];
+		}
+
+		return "";
+	}
 	
 	string postLogin(string username, string password)
 	{
 		auto rows = db.query("select user_id from users where username = ? and password = ?", username, password);
+
 		if (rows.length == 0)
 			return "";
+
 		string cookie = getSalt();
 		cookies[cookie] = rows.front()["user_id"].to!int;
+
 		return cookie;
+	}
+
+	void postLogout(string auth)
+	{
+		string sess = parseCookie(auth, "sess");
+		writeln(sess);
+		cookies.remove(sess);
+
+		writeln("\nCookies:");
+		foreach(string c, int i; cookies)
+			writeln(c);
 	}
 }
