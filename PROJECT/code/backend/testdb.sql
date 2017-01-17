a66839bdd63d512143bee065f01bdffe2f416340
Eupheme <hannarainbowmail@gmail.com>
2017-01-17 14:20:47 +0100

    added testing

---
+++ PROJECT/code/backend/testdb.sql
@@ -0,0 +1,122 @@
+USE biblios;
+
+INSERT INTO subjects(name) VALUES ("fantasy");
+
+INSERT INTO subjects(name) VALUES ("horror");
+
+
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("Eragon", "Christopher Paolini", 1082407047, "2003-08-26", "2016-11-12", 1, "One boy . . . One dragon . . . A world of adventure. When Eragon finds a polished stone in the forest, he thinks it is the lucky discovery of a poor farm boy. But when the stone brings a dragon hatchling, Eragon soon realizes he has stumbled upon a legacy nearly as old as the Empire itself. Overnight his simple life is shattered and he is thrust into a perilous new world of destiny, magic and power. Can Eragon take up the mantle of the legendary Dragon Riders? The fate of the Empire may rest in his hands.", 0, "item_1");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("Eldest", "Christopher Paolini", 0781434041, "2005-08-23", "2016-11-12", 1, "Eragon and his dragon, have just saved the rebel state from destruction by the forces of King Galbatorix. Now, Eragon must travel to Ellesmera for further training in magic. Meanwhile, his cousin Roran must fight a new battle back home in Carvahall - one that puts Eragon in even graver danger.", 0, "item_2");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("Brisingr", "Christopher Paolini", 0746407044, "2008-09-20", "2016-10-02", 1, "It has been only months since Eragon first uttered brisingr, an ancient language term for fire. Since then, he is not only learned to create magic with words — he has been challenged to his very core. Following the colossal battle against the Empires warriors on the Burning Plains, Eragon and his dragon, Saphira, have narrowly escaped with their lives. Still, there is more adventure at hand for the Rider and his dragon, as Eragon finds himself bound by a tangle of promises he may not be able to keep.", 0, "item_3");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("The Magicians", "Lev Grossman", 1083104009, "2009-05-04", "2016-12-01", 1, "In a secret world of forbidden knowledge, power comes at a terrible price...Quentin Coldwaters life is changed forever by an apparently chance encounter: when he turns up for his entrance interview to Princeton he finds his interviewer dead - but a strange envelope bearing Quentins name leads him down a very different path to any hed ever imagined. ", 0, "item_4");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("Fellowship of the Ring", "J. R. R. Tolkien", 0780007123, "2002-01-17", "2016-01-29", 1, "Frodo Baggins knew the Ringwraiths were searching for him—and the Ring of Power he bore that would enable Sauron to destroy all that was good in Middle-earth. Now it was up to Frodo and his faithful servant Sam to carry the Ring to where it could be destroyed—in the very center of Saurons dark kingdom.", 0, "item_5");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("Finders Keepers", "Stephen King", 0111473698, "2015-06-02", "2016-04-12", 2, "Pete Saubers, a boy whose father was brutally injured by a stolen Mercedes, discovers a buried trunk containing cash and Rothsteins notebooks. After thirty-five years in prison, Morris is up for parole. And he is hell-bent on recovering his treasure. Now it is up to retired detective Bill Hodges - running an investigative company called Finders Keepers - to rescue Pete from an ever-more deranged and vengeful Morris...", 0, "item_6");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("A Game Of Thrones", "George R. R. Martin", 1480007548, "1996-08-01", "2016-04-12", 1, "Summers span decades. Winter can last a lifetime. And the struggle for the Iron Throne has begun. As Warden of the north. Lord Eddard Stark counts it a curse when King Robert bestows on him the office of the Hand. His honour weighs him down at court where a true man does what he will. not what he must ... and a dead enemy is a thing of beauty. The old gods have no power in the south. Starks family is split and there is treachery at court. Worse. the vengeance-mad heir of the deposed Dragon King has grown to maturity in exile in the Free Cities. He claims the Iron Throne.", 0, "item_7");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("It", "Stephen King", 1381444707, "1986-09-13", "2016-04-12", 2, "A promise made twenty-eight years ago calls seven adults to reunite in Derry, Maine, where as teenagers they battled an evil creature that preyed on the citys children. Unsure that their Losers Club had vanquished the creature all those years ago, the seven had vowed to return to Derry if IT should ever reappear. Now, children are being murdered again and their repressed memories of that summer return as they prepare to do battle with the monster lurking in Derrys sewers once more.", 0, "item_8");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("The Magician King", "Lev Grossman", 1180099553, "2011-08-09", "2016-04-12", 1, "Quentin Coldwater should be happy. He escaped a miserable Brooklyn childhood, matriculated at a secret college for magic, and graduated to discover that Fillory was actually real. But even as a Fillorian king, Quentin finds little peace. His old restlessness returns, and he longs for the thrills a heroic quest can bring. Accompanied by his oldest friend, Julia, Quentin sets off - only to somehow wind up back in the real world and not in Fillory, as they had hoped. As the pair struggle to find their way back to their lost kingdom, Quentin is forced to rely on Julias illicitly-learned sorcery as they face a sinister threat in a world very far from the beloved fantasy novels of their youth.", 0, "item_9");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("Pet Sematary", "Stephen King", 1081444708, "1983-11-14", "2016-02-22", 2, "The house looked right. Felt right to the Dr. Louis Creed.Rambling. Old the unsmart and comfortable. A place where the family could settle; the children grow and play and explore. The rolling hills and meadows of Maine seemed a world away from the fume-choked dangers of Chicago.Only the occasional big truck out on the two-lane highway. grinding up through the gears. hammering down the long gradients. growled out an intrusive threat.But behind the house and far away from the road: that was safe. Just a carefully cleared path up into the woods where generations of local children have processed with the solemn innocence of the young. taking with them their dear departed pets for burial.A sad place maybe. but safe. Surely a safe place. Not a place to seep into your dreams. to wake you. sweating with fear.", 0, "item_10");
+
+INSERT INTO items (title, author, isbn, published_date, added_date, subject,
+description, status, picture) VALUES ("Revival", "Stephen King", 0881444789, "2014-11-03", "2016-03-01", 2, "A dark and electrifying novel about addiction, fanaticism, and what might exist on the other side of life.", 0, "item_11");
+
+
+
+
+INSERT INTO users (first_name, last_name, username, email, password, salt, status,
+added_date) VALUES ("firstname", "lastname", "User5", "guest@email.com", "5c5afc2440e18789664d73cb53fbe36d71bd42", "p7IPzX8gPxbNjKkz2cmC1FHiCKtQTpgE", 0, "2016-12-13");
+
+INSERT INTO users (first_name, last_name, username, email, password, salt, status,
+added_date, picture) VALUES ("Alexia", "Reed", "alexia-red11", "alexiared@email.com", "d768cddf8086d991c455fc59ad8db66dde0a16b", "tFWPfM0egJO5t27rMiOKTGlNaskf7eJU", 0, "2016-11-14", "user_3");
+
+INSERT INTO users (first_name, last_name, username, email, password, salt, status,
+added_date, picture) VALUES ("Alison", "Clarke", "aly2cyn", "alycyn@email.com", "beffc876795dae876f73c3ff1b9fcc3a9a36f56", "58o18KsWUi9d9dL5jgJckInK2OtxZWlE", 0, "2016-12-01", "user_4");
+
+INSERT INTO users (first_name, last_name, username, email, password, salt, status,
+added_date, picture) VALUES ("Brock", "Turner", "bhodiir3k", "brock3i@email.com", "7ade1e666cf5be4362764424c9e7b0c37b32c61d", "W252lMFe8AxQclijLfoTI3cQE5wMXJnZ", 0, "2016-10-13", "user_5");
+
+INSERT INTO users (first_name, last_name, username, email, password, salt, status,
+added_date) VALUES ("Lara", "Mojškrc", "L4rchy", "l4rchy@email.com", "dd1a6527be314ae4a8c4b436326af5e2db29fa6e", "eGglkcbCEX79FPsLX8nGDI8tmye5QKLv", 2, "2016-12-29");
+
+INSERT INTO users (first_name, last_name, username, email, password, salt, status,
+added_date) VALUES ("Den", "J.", "deriki5u", "drawingden@email.com", "69747051568ed014fece445c64cc728fd1df34b7", "zhQkt4mkdNdRLMqF4aug1jME7OZju6Sp", 2, "2017-01-13");
+
+INSERT INTO users (first_name, last_name, username, email, password, salt, status,
+added_date) VALUES ("Sara", "Danver", "danvver6", "SDanver@email.com", "d2b46a4f38a4cffb52ed7c9375dcc4cfe721da", "Ui0J4q64gBLtouDKX6tYTHDxtXN1FZ48", 2, "2017-01-04");
+
+
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(2, 6, "2016-12-22", "2016-12-25");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(2, 7, "2016-12-22", "2016-12-30");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(2, 8, "2016-12-22", "2016-12-30");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(2, 1, "2017-01-10", NULL);
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(2, 3, "2017-01-12", NULL);
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(2, 5, "2016-12-30", NULL);
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(2, 8, "2017-01-15", NULL);
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(1, 2, "2016-11-22", "2016-12-01");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(1, 3, "2016-11-22", "2016-12-03");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(1, 8, "2016-12-31", "2017-01-14");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(1, 11, "2017-01-11", NULL);
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(4, 4, "2016-12-01", "2016-12-07");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(4, 5, "2016-12-07", "2016-12-15");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(1, 2, "2017-01-07", "2016-12-15");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(5, 6, "2016-11-13", "2016-11-24");
+
+INSERT INTO transactions (user_id, item_id, borrowed_date, returned_date) VALUES
+(5, 7, "2016-10-22", "2016-11-03");
+
+
+
+INSERT INTO requests (user_id, title, author, requested_date, subject, description) VALUES (2, "Inheritance", "Christopher Paolini", "2016-12-22", 1, "It began with Eragon ...It ends with Inheritance. t so very long ago, Eragon - Shadeslayer, Dragon Rider - was nothing more than a poor farm boy, and his dragon, Saphira, only a blue stone in the forest. w, the fate of an entire civilization rests on their shoulders. The Rider and his dragon have come further than anyone dared to imagine. But can they topple the evil king, Galbatorix, and restore justice to Alagaesia?And if so, at what cost?");
+
+INSERT INTO requests (user_id, title, author, requested_date, subject, description) VALUES (5, "Midnight", "Dean Koontz", "2017-01-12", 2, "What is the dark secret that haunts Moonlight Cove? A string of inexplicable deaths has occurred in this picturesque coastal town; sinister, shadowy figures stalk the streets in the dead of night and four people are drawn together by terrifying circumstance: A young woman determined to find the truth behind her sisters suicide.");
+
+
